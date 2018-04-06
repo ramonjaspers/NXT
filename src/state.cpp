1fa92060b7c5b9ae699fe4e3782f529400889a79
Brandon Kroes <b.kroes@protonmail.com>
2018-04-06 16:39:47 +0200

    closes #4

--- src/state.cpp
+++ src/state.cpp
@@ -6,52 +6,50 @@
 #include <iostream>
 
 // The different types of states are 
-State::State()
-{
-	Log log;
-	Init it;
+State::State() {
+    Log log;
+    Init it;
 
-	this->init = it;
+    this->init = it;
 
-	this->motion = this->init.get_motion();
-	this->sensor = this->init.get_sensor();
+    this->motion = this->init.get_motion();
+    this->sensor = this->init.get_sensor();
 }
 
-Motion State::getMotion(){
-	return this->motion;
+Motion State::getMotion() {
+    return this->motion;
 }
 
-Sensor State::getSensor(){
-	return this->sensor;
+Sensor State::getSensor() {
+    return this->sensor;
 }
 
 // Driving straight, everything is going good
-void State::clear(){
-    if(this->sensor.both_white() && this->sensor.object_near()){
+void State::clear() {
+    if (this->sensor.both_white() && this->sensor.object_near()) {
         //drive along
-    }else{
+    } else {
         object_or_turn();
     }
 }
 
 
-
 // Driving slower and the ultrasonic sensor needs to be in 'avoid', 'check if I'm past' mode etc
-bool State::obstacle_avoid(){
-	return true;
+bool State::obstacle_avoid() {
+    return true;
 }
 
 // Driving slower and taking a turn
-void State::turn(){
+void State::turn() {
     // IR Left
     // RGB Right
 
-    while(!this->sensor.both_white()){
-        if(this->sensor.object_near()){
+    while (!this->sensor.both_white()) {
+        if (this->sensor.object_near()) {
             obstacle_avoid();
-        }else if(this->sensor.IR_black_RGB_white()){
+        } else if (this->sensor.IR_black_RGB_white()) {
             //TODO: turn left
-        }else if(this->sensor.RGB_black_IR_white()){
+        } else if (this->sensor.RGB_black_IR_white()) {
             //TODO: Turn right
         }
     }
@@ -59,11 +57,10 @@ void State::turn(){
 }
 
 // driving through a grid storing the directions driven.
-bool State::grid_travel(){
-	return true;
+bool State::grid_travel() {
+    return true;
 }
 
-State::~State()
-{
+State::~State() {
 }
 
