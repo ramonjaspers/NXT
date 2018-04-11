529c815f1ebfb976d9eb10a0c7196daa4d026a8f
MartyVos <31736433+MartyVos@users.noreply.github.com>
2018-04-11 13:09:09 +0200

    MVP to OOP

--- src/ultrasonic.h
+++ src/ultrasonic.h
@@ -6,18 +6,21 @@
 #ifndef SRC_ULTRASONIC_H
 #define SRC_ULTRASONIC_H
 
+#include "BrickPi3.h"
+
 
 class Ultrasonic {
 
 private:
     // TODO: cast to 8 bit
-    unsigned int detected;
+    BrickPi3 Brick;
+    uint8_t PORT;
+    sensor_ultrasonic_t Sonic;
+    float detected;
     std::vector <int> detection_range;
 
-
-
 public:
-    Ultrasonic();
+    Ultrasonic(uint8_t port);
     bool object_in_range();
     void get_distance();
     ~Ultrasonic();
