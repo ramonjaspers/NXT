b52aca83b56bd2761fc69bdb577874ade1412e86
MartyVos <31736433+MartyVos@users.noreply.github.com>
2018-04-11 13:08:37 +0200

    MVP to OOP

--- src/ultrasonic.cpp
+++ src/ultrasonic.cpp
@@ -3,23 +3,31 @@
 //
 
 #include "ultrasonic.h"
+#include <iostream>
+#include <unistd.h>
 
-
-Ultrasonic::Ultrasonic(){
-
+Ultrasonic::Ultrasonic(uint8_t port){
+    BrickPi3 BP;
+    this->Brick = BP;
+    this->PORT = port;
+    this->Brick.set_sensor_type(this->PORT, SENSOR_TYPE_NXT_ULTRASONIC);
+    sensor_ultrasonic_t Sonic3;
+    this->Sonic = Sonic3;
 }
 
 
 bool Ultrasonic::object_in_range(){
-
+        // TODO: own detection
     return ( this->detected >= this->detection_range[0] && this->detected <= this->detection_range[1] );
 
 }
 
 void Ultrasonic::get_distance(){
-
     // code that extracts the range, stores it in detected
     //return this->detected;
+    this->Brick.get_sensor(this->PORT, this->Sonic);
+    this->detected = this->Sonic.cm;
+    std::cout << this->Sonic.cm << std::endl;
 }
 
 Ultrasonic::~Ultrasonic(){
